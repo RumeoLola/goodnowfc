4b0ae8c9d24aa65d0af9639783088296adcfe3bf
RumeoLola <rumeomololaz@gmail.com>
2022-05-13 02:03:22 -0400

    trying new color theme

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ function openNav() {
   document.getElementById("mySidenav").style.display = "block";
   document.getElementById("nosite").style.display = "none";
   document.getElementById("daeh").style.marginBottom = "325px";
+  document.getElementById("daeh").style.borderBottom = "none"
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.display = "none";
   document.getElementById("daeh").style.marginBottom = "0";
   document.getElementById("nosite").style.display = "flex";
+  document.getElementById("daeh").style.borderBottom = "2px solid #999"
 }
