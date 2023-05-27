fbb99876c4347969ea46dd8a7567939812818354
RumeoLola <rumeomololaz@gmail.com>
2023-05-26 22:42:42 -0400

    removed python virtual environment, want to try something different

--- djangoutilitytocreatedatabasetable.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# create database tables automatically
-# migrate command migrates to existing tables and
-# can create new tables that don't already exist
-python app.py makemigrations
-python app.py migrate
