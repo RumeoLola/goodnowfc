bb8f12d55867af42b84bb078c6b54f73ccb6446c
RumeoLola <rumeomololaz@gmail.com>
2023-05-27 00:32:27 -0400

    unobstrusive event binding in use

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+document.addEventListener("DOMContentLoaded",
+  function  (event) {
+    function  sendIt()  {
+      var myName = document.getElementById("name").value;
+      var myEmail = document.getElementById("email").value;
+      var mySubject = document.getElementById("subject").value;
+      var myMessage = document.getElementById("message").value;
+      console.log("The following message: " + "'" + myMessage + "'" + " is from " + myName + " is about " + mySubject + " The sender's address is " + myEmail)
+    }
+    document.querySelector("button").addEventListener("click", sendIt);
+  }
+);
+
 function openNav() {
   document.getElementById("mySidenav").style.display = "block";
   document.getElementById("nosite").style.display = "none";
